57fa2058f75f0623406e803105af8db10240b64b
Avanish Kumar <ak2729@srmist.edu.in>
2025-09-18 23:44:03 +0530

    Added productController

---
+++ backend/controllers/productController.js
@@ -0,0 +1,33 @@
+// backend/controllers/productController.js
+
+import Product from '../models/productModel.js';
+
+export const createProduct = async (req, res) => {
+    try {
+        const {name, productId, description } = req.body;
+        const productExist = await Product.findOne({ productId });
+
+        if(productExist) {
+            return res.status(400).json({ message : 'Product with this ID already exist'});
+        }
+
+        const product = await Product.create({name, productId, description });
+        res.status(201).json(product);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+export const verifyProduct = async (req, res) => {
+    try {
+        const product = await Product.findOne({productId: req.param.productId });
+
+        if(product) {
+            res.status(200).json({ product });
+        }else{
+            res.status(400).json({message: 'Product not found'});
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message })
+    }
+};
