47568a74ece8ffe92777aa6bf3e2ee3ed16fbd5f
Avanish Kumar <ak2729@srmist.edu.in>
2025-09-24 23:16:59 +0530

    Updated App.jsx for usage of these pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,29 @@
-// src/App.jsx
+// frontend/src/App.jsx
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ScanPage from './pages/ScanPage';
+
+// Import Components
+import Header from './components/Header';
+
+// Import Pages
+import HomePage from './pages/HomePage';
+import AddProductPage from './pages/AddProductPage';
+import SellerPage from './pages/SellerPage';
+import ScanPage from './pages/ScanPage'; // This is our Consumer page
 import ResultPage from './pages/ResultPage';
 
 function App() {
   return (
     <Router>
       <div className="bg-gray-900 min-h-screen text-white font-sans">
-        <main className="container mx-auto p-4">
+        <Header />
+        <main className="container mx-auto p-4 mt-8">
           <Routes>
-            <Route path="/" element={<ScanPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/manufacturer" element={<AddProductPage />} />
+            <Route path="/seller" element={<SellerPage />} />
+            <Route path="/consumer" element={<ScanPage />} />
             <Route path="/verify/:productId" element={<ResultPage />} />
           </Routes>
         </main>
