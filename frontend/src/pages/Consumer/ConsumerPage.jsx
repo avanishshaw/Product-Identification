490f70d748f0c42c9c19a8d7106f900da010c5d8
Avanish Kumar <ak2729@srmist.edu.in>
2025-09-29 23:39:32 +0530

    Added ConsumerPage

---
+++ frontend/src/pages/Consumer/ConsumerPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ConsumerPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[80vh]">
+      <h1 className="text-4xl md:text-5xl font-bold mb-8 text-cyan-400">
+        Consumer Page
+      </h1>
+      <p className="text-lg text-gray-300">
+        Here consumers can scan and verify products.
+      </p>
+    </div>
+  );
+};
+
+export default ConsumerPage;
