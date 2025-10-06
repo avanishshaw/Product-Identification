8941a9577a1a2f934c60b83e7bc66a78779a5d6e
Avanish Kumar <ak2729@srmist.edu.in>
2025-10-06 23:17:31 +0530

    Updated sellerModel.js

--- backend/models/sellerModel.js
+++ backend/models/sellerModel.js
@@ -1,17 +1,14 @@
-// backend/models/sellerModel.js
-
 import mongoose from 'mongoose';
 
 const sellerSchema = new mongoose.Schema({
-  sellerName: { type: String, required: true },
-  sellerBrand: { type: String },
-  sellerCode: { type: String, required: true, unique: true },
-  sellerPhoneNumber: { type: String },
-  sellerManager: { type: String },
-  sellerAddress: { type: String, required: true },
-  manufacturerId: { type: String, required: true },
+    sellerCode: { type: String, required: true, unique: true },
+    sellerName: { type: String, required: true },
+    sellerBrand: { type: String },
+    sellerPhoneNumber: { type: String },
+    sellerManager: { type: String },
+    sellerAddress: { type: String, required: true },
+    manufacturerId: { type: String, required: true }, // The Manufacturer who registered this seller
 }, { timestamps: true });
 
 const Seller = mongoose.model('Seller', sellerSchema);
-
 export default Seller;
