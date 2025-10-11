daf5d511b5132c7aaa81ff465f6e5cae6b6f6ffa
Avanish Kumar <ak2729@srmist.edu.in>
2025-10-11 23:58:47 +0530

    Added UserModel

---
+++ backend/models/userModel.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['Manufacturer', 'Seller', 'Consumer'],
+    },
+    // You can add more fields like name, company code, etc. here
+    name: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+
+// This function runs before a new user is saved to the database
+// It hashes the password so we never store plain text passwords
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// This function allows us to compare an entered password with the stored hashed password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
